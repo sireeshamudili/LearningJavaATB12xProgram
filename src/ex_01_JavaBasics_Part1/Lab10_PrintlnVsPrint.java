b4acb5873ad0ddeb7ed6ac06afb5832a5cdd486b
Sireesha <sireeshamudili580@gmail.com>
2025-05-11 17:59:22 +0530

    Core Java

--- src/ex_01_JavaBasics_Part1/Lab10_PrintlnVsPrint.java
+++ src/ex_01_JavaBasics_Part1/Lab10_PrintlnVsPrint.java
@@ -2,9 +2,11 @@ package ex_01_JavaBasics_Part1;
 
 public class Lab10_PrintlnVsPrint {
     public static void main(String[] args) {
+        // println adding new line automatically
         System.out.println("Hi");
-        System.out.println("Hello"); //Give in new line
+        System.out.println("Hello");
 
+        // print does not add new line in this case output attach in one line
         System.out.print("Hi");
         System.out.print("Hello"); //Gives in the same line
     }
---
+++ src/exercises/Task_001.java
@@ -0,0 +1,19 @@
+package exercises;
+
+public class Task_001 {
+    public static void main(String[] args) {
+        int num=5;
+
+        //printf used adding formatting style in output
+        System.out.printf("5*1 = %d\n" ,5*1);
+        System.out.printf("5*2 = %d\n" ,5*2);
+        System.out.printf("5*3 = %d\n" ,5*3);
+        System.out.printf("5*4 = %d\n" ,5*4);
+        System.out.printf("5*5 = %d\n" ,5*5);
+        System.out.printf("5*6 = %d\n" ,5*6);
+        System.out.printf("5*7 = %d\n" ,5*7);
+        System.out.printf("5*8 = %d\n" ,5*8);
+        System.out.printf("5*9 = %d\n" ,5*9);
+        System.out.printf("5*10 = %d\n" ,5*10);
+    }
+}
