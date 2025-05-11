5b4cc56e329c13d1ec3d843a08faedee963a6f9f
Sireesha <sireeshamudili580@gmail.com>
2025-05-11 17:32:01 +0530

    Core Java

---
+++ src/ex_03_Literals/Lab20_Literals.java
@@ -0,0 +1,39 @@
+package ex_03_Literals;
+
+public class Lab20_Literals {
+    public static void main(String[] args) {
+        int age = 65;
+        // Literal - Integral
+
+        final float pi = 3.14f;
+        //  Literal - float
+
+        final int girl_age = 18;
+        //girl_age = 19;
+
+        System.out.println(girl_age);
+
+        //--------------------
+        int Enum = 9; // enum is keyword
+        System.out.println(Enum);
+
+        //--------------------
+        int age2 = 65; // Decimal System - base wil 10
+
+        // Binary Literal
+        int binary_num = 0b1010; // Decimal System - 10
+
+        // Octal base? -> 8
+        int octal = 0101;
+
+        // Hexadecimal Literals - 16
+        int hex = 0Xface;
+        //base 16 color combination are in hex - #28a745
+
+        //-------------------
+        boolean is_married = true;
+        boolean is_married_amit = false;
+        //boolean is_married_amit = 123;
+    }
+
+}
---
+++ src/ex_03_Literals/Lab21_Char_Literals.java
@@ -0,0 +1,46 @@
+package ex_03_Literals;
+
+public class Lab21_Char_Literals {
+    public static void main(String[] args) {
+
+        char c1 = 'A'; // A to Z, a-z, !@#$%^&*()_+
+        //char c = "A"; // "" == String = Bunch of Char
+
+        char c2 = 'B';
+        char c3 = '@';
+        char c4 = '_';
+        char c5 = '9';
+        char c6 = '1';
+        char c7 = '(';
+        char c8 = ' '; // blank space
+
+        // Escape Char
+        char new_line = '\n';
+        char tab_line = '\t';
+        char back_space = '\b';
+        char carriage_return = '\r';
+
+        System.out.println("Sireesha");
+        System.out.println("Siri"+new_line+"M");
+        System.out.println("Siri\nM");
+        System.out.println("Siri"+tab_line+"M");
+        System.out.println("Siri"+back_space+"M");
+        System.out.println( " ----- ");
+
+        System.out.println("Siri is "+carriage_return+"M");
+
+        System.out.println("Hi, This is a First line"+new_line+"This is second line\n This is Third line");
+
+
+        char c10 = 'A';
+        // ASCII, (limited numbers) - A -> 65
+
+        char rupees = '₹';
+        System.out.println(rupees);
+
+        char my_laugh_smiley = '\u1f60'; // :)
+        System.out.println(my_laugh_smiley);
+
+        char c11  = '\u1F60';
+    }
+}
