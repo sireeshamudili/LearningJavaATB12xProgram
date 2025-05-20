bb6b5924bce6d69383d01beb3debb28e1990d6b3
Sireesha <sireeshamudili580@gmail.com>
2025-05-20 10:15:07 +0530

    Core Java

--- src/exercises/Task_001.java
+++ src/exercises/Task_001.java
@@ -5,15 +5,15 @@ public class Task_001 {
         int num=5;
 
         //printf used adding formatting style in output
-        System.out.printf("5*1 = %d\n" ,5*1);
-        System.out.printf("5*2 = %d\n" ,5*2);
-        System.out.printf("5*3 = %d\n" ,5*3);
-        System.out.printf("5*4 = %d\n" ,5*4);
-        System.out.printf("5*5 = %d\n" ,5*5);
-        System.out.printf("5*6 = %d\n" ,5*6);
-        System.out.printf("5*7 = %d\n" ,5*7);
-        System.out.printf("5*8 = %d\n" ,5*8);
-        System.out.printf("5*9 = %d\n" ,5*9);
-        System.out.printf("5*10 = %d\n" ,5*10);
+        System.out.printf("%dx1=%d\n" ,num,num*1);
+        System.out.printf("%dx2=%d\n" ,num,num*2);
+        System.out.printf("%dx3=%d\n" ,num,num*3);
+        System.out.printf("%dx4=%d\n" ,num,num*4);
+        System.out.printf("%dx5=%d\n" ,num,num*5);
+        System.out.printf("%dx6=%d\n" ,num,num*6);
+        System.out.printf("%dx7=%d\n" ,num,num*7);
+        System.out.printf("%dx8=%d\n" ,num,num*8);
+        System.out.printf("%dx9=%d\n" ,num,num*9);
+        System.out.printf("%dx10=%d\n" ,num,num*10);
     }
 }
